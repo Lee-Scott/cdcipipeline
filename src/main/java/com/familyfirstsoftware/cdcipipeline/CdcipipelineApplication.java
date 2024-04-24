b54c6666d5618cc1f385e3eb8422d7327d464c2e
Lee-Scott <leehscott24@gmail.com>
2024-04-23 18:36:16 -0700

    first commit

---
+++ src/main/java/com/familyfirstsoftware/cdcipipeline/CdcipipelineApplication.java
@@ -0,0 +1,13 @@
+package com.familyfirstsoftware.cdcipipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcipipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcipipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/familyfirstsoftware/cdcipipeline/CdcipipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyfirstsoftware.cdcipipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcipipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
